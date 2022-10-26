307050d7c8c0bff2c42478476fea6787af30bb74
jakub <>
2022-10-27 00:18:07 +0200

    example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 const routes: Routes = [
   { path: '', component: FirstComponent },
